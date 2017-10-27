ebca6be560a8531d5abaa992b7d94604eca1615e
dannypsnl <dannypsnl@gmail.com>
2017-10-27 18:25:32 +0800

    Update bindings example

--- haskell-learn/basic/bindings.hs
+++ haskell-learn/basic/bindings.hs
@@ -2,8 +2,13 @@ module Main where
 x::Int
 x = 2
 z::Int
-z = x + 1
+z = x + 1 -- z = 3
+
+res = let y = 2
+          k = z -- k = 3
+      in y * k -- 6
 
 main::IO()
 main = do
   print z
+  print res
