8bfc812f9d89f5f0abca908c033d77c51650a153
dannypsnl <dannypsnl@gmail.com>
2017-12-31 01:51:58 +0800

    add comment explain code

--- c++/opencv-play/show_img.cpp
+++ c++/opencv-play/show_img.cpp
@@ -11,7 +11,10 @@ int main(int argc, char **argv) {
     std::cout << "Don't have this image" << std::endl;
     return -1; // exit with signal 1
   }
+  // Create a window
   cv::namedWindow("Show Image", cv::WINDOW_AUTOSIZE);
+  // Wait a keyboard's input
   cv::waitKey(0);
+  // Destroy the window by ref name
   cv::destroyWindow("Show Image");
 }
