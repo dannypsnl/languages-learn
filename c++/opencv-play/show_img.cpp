cfa252d190b048559be117d62d699918c6c4acfa
dannypsnl <dannypsnl@gmail.com>
2017-12-31 01:34:42 +0800

    basic openCV example

--- c++/opencv-play/show_img.cpp
+++ c++/opencv-play/show_img.cpp
@@ -1,3 +1,11 @@
-#include "opencv2/opencv.hpp"
+#include <iostream>
+#include <opencv2/opencv.hpp>
 
-int main() {}
+int main(int argc, char **argv) {
+  // read image from argument
+  cv::Mat img = cv::imread(argv[1], -1);
+  if (img.empty()) {
+    std::cout << "Don't have this image" << std::endl;
+    return -1; // exit with signal 1
+  }
+}
