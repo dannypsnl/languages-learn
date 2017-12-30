843e82319eabe8edf9fba99a10fa6093cffafbc7
dannypsnl <dannypsnl@gmail.com>
2017-12-31 01:50:17 +0800

    complete show_img & record how to start it & download image

--- c++/opencv-play/show_img.cpp
+++ c++/opencv-play/show_img.cpp
@@ -1,3 +1,6 @@
+// compile:
+//   clang++ -std=c++14 show_img.cpp -lopencv_core -lopencv_highgui
+//   -lopencv_imgcodecs
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
@@ -8,4 +11,7 @@ int main(int argc, char **argv) {
     std::cout << "Don't have this image" << std::endl;
     return -1; // exit with signal 1
   }
+  cv::namedWindow("Show Image", cv::WINDOW_AUTOSIZE);
+  cv::waitKey(0);
+  cv::destroyWindow("Show Image");
 }
