2b3ba8c5beb520385d7e472035bbc028330a54d4
dannypsnl <dannypsnl@gmail.com>
2018-01-06 00:00:20 +0800

    add comment for afile_server

--- erlang-learn/afile_server.erl
+++ erlang-learn/afile_server.erl
@@ -1,8 +1,12 @@
 -module(afile_server).
 -export([start/1, loop/1]).
 
+% spawn($module, $function, [argument...])
 start(Dir) -> spawn(afile_server, loop, [Dir]).
 
+% loop function
+%  receive: `list_dir` atom return file list in the directory.
+%  receive: {`get_file` atom, $File} return file by $Full path(we append it from $Dir & $File)
 loop(Dir) ->
     receive
         {Client, list_dir} ->
