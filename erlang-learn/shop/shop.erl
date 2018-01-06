cf2d046e2821a925d508cdfab6feab0121b9f13e
dannypsnl <dannypsnl@gmail.com>
2018-01-06 23:06:33 +0800

    create test

--- erlang-learn/shop/shop.erl
+++ erlang-learn/shop/shop.erl
@@ -1,8 +1,16 @@
 -module(shop).
--export([cost/1]).
+-export([test/0, cost/1]).
 
 cost(oranges)   -> 5;
 cost(newspaper) -> 8;
 cost(apples)    -> 2;
 cost(pears)     -> 9;
 cost(milk)      -> 7.
+
+test() ->
+    5 = cost(oranges),
+    8 = cost(newspaper),
+    2 = cost(apples),
+    9 = cost(pears),
+    7 = cost(milk),
+    test_pass.
