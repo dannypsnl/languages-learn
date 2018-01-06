ab00c06a9e6dd8eaf05b3a28e52fb9f8787b8f58
dannypsnl <dannypsnl@gmail.com>
2018-01-06 23:02:18 +0800

    comment & clean up geometry.erl

--- erlang-learn/geometry/geometry.erl
+++ erlang-learn/geometry/geometry.erl
@@ -1,11 +1,13 @@
 -module(geometry).
 -export([test/0, area/1]).
 
+% `;` spread match rule
+% `.` represent end of the rule
 area({rectangle, Width, Height}) -> Width * Height;
 area({circle, Radius})           -> Radius * Radius * 3.1415926;
 area({square, Side})             -> Side * Side.
 
-test() -> 
+test() ->
     12 = area({rectangle, 4, 3}),
     400 = area({square, 20}),
     tests_work.
