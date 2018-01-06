0cf7a052db515b165df329102bee00200713a59b
dannypsnl <dannypsnl@gmail.com>
2018-01-06 10:54:32 +0800

    Add comment for Select.cpp

--- c++/template/Select.cpp
+++ c++/template/Select.cpp
@@ -8,7 +8,9 @@ template <typename T, typename U> struct Select<false, T, U> {
 };
 
 int main() {
+  // false, so we will Select int
   Select<false, char, int>::Result i{1};
+  // true, so we will Select char
   Select<true, char, int>::Result c{'c'};
   std::cout << "Select<true, char, int> c = " << c << '\n'
             << "Select<false, char, int> i = " << i << std::endl;
