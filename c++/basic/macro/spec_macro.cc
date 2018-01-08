d50e953859bbc2f47de5d7e8fd36344c3ea60fb6
dannypsnl <dannypsnl@gmail.com>
2018-01-08 14:53:30 +0800

    __FILE__

--- c++/basic/macro/spec_macro.cc
+++ c++/basic/macro/spec_macro.cc
@@ -5,4 +5,6 @@ int main() {
   std::cout << __LINE__ << std::endl;
   // Print this function ref name
   std::cout << __func__ << std::endl;
+  // Print the source file name
+  std::cout << __FILE__ << std::endl;
 }
