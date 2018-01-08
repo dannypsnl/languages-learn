0d323464a29aa140c9e74ac798aed5ebe06622a9
dannypsnl <dannypsnl@gmail.com>
2018-01-08 15:12:06 +0800

    spread to two module

--- erlang-learn/fac/main.erl
+++ erlang-learn/fac/main.erl
@@ -1,11 +1,11 @@
 -module(main).
 -export([main/1]).
 
+-import(fac, [fac/1]).
+
 main([A]) ->
     I = list_to_integer(A),
-    F = fac(I),
+    F = fac:fac(I),
     io:format("factorial ~w = ~w~n", [I, F]),
     init:stop().
 
-fac(0) -> 1;
-fac(N) -> N*fac(N-1).
