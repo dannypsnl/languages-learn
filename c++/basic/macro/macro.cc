988db776418c52af219c4a0e8f11f84d56aec0f9
dannypsnl <dannypsnl@gmail.com>
2018-01-08 14:47:10 +0800

    add comment to explain

--- c++/basic/macro/macro.cc
+++ c++/basic/macro/macro.cc
@@ -1,10 +1,10 @@
 #include <iostream>
 
-#define NEW_FUNC(__name, __id)                                                 \
+#define NEW_FUNC(__name, __id) /* append __name and __id */                    \
   void __name##__id() { std::cout << "func: " << __id << std::endl; }
 
-NEW_FUNC(foo, 1)
-NEW_FUNC(foo, 2)
+NEW_FUNC(foo, 1) // foo1
+NEW_FUNC(foo, 2) // foo2
 
 int main() {
   foo1();
