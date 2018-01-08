ec4dc7d7376005bc003db45467fd7fba2f94e615
dannypsnl <dannypsnl@gmail.com>
2018-01-08 11:45:24 +0800

    create List use Node

--- c++/data-structure/list/list.h
+++ c++/data-structure/list/list.h
@@ -1,13 +1,19 @@
+#include <initializer_list>
+#include <iostream>
+
 template <class T> class Node {
   T value;
   Node *next_node = nullptr;
 
 public:
+  Node();
   Node(T);
   void next(Node<T> *);
   T get_value();
   Node<T> *get_next();
 };
+
+template <class T> Node<T>::Node() {}
 
 template <class T> Node<T>::Node(T v) : value{v} {}
 
@@ -16,3 +22,26 @@ template <class T> void Node<T>::next(Node *n) { this->next_node = n; }
 template <class T> T Node<T>::get_value() { return value; }
 
 template <class T> Node<T> *Node<T>::get_next() { return next_node; }
+
+template <class T> class List {
+  Node<T> *first = new Node<T>();
+
+public:
+  List(std::initializer_list<T>);
+  void for_each();
+};
+
+template <class T> List<T>::List(std::initializer_list<T> l) {
+  first = new Node<T>(*l.begin());
+  auto now = first;
+  for (auto iter = l.begin(); iter != l.end(); ++iter) {
+    now->next(new Node<T>(*iter));
+    now = now->get_next();
+  }
+}
+
+template <class T> void List<T>::for_each() {
+  for (auto now = first; now->get_next() != nullptr; now = now->get_next()) {
+    std::cout << now->get_value() << std::endl;
+  }
+}
