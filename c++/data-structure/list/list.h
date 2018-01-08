d143860a884ce43fc63b2084bfb5862687d65181
dannypsnl <dannypsnl@gmail.com>
2018-01-08 11:58:21 +0800

    clean up

--- c++/data-structure/list/list.h
+++ c++/data-structure/list/list.h
@@ -4,6 +4,7 @@
 namespace collec {
 
 namespace {
+
 template <class T> class Node {
   T value;
   // next_node default is nullptr
@@ -27,6 +28,7 @@ template <class T> void Node<T>::next(Node *n) { this->next_node = n; }
 template <class T> T Node<T>::get_value() { return value; }
 
 template <class T> Node<T> *Node<T>::get_next() { return next_node; }
+
 } // namespace
 
 template <class T> class List {
@@ -47,6 +49,7 @@ template <class T> List<T>::List(std::initializer_list<T> l) {
   }
 }
 
+// TODO: accept functor at for_each
 template <class T> void List<T>::for_each() {
   for (auto now = first; now->get_next() != nullptr; now = now->get_next()) {
     std::cout << now->get_value() << std::endl;
