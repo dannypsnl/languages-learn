b12ab907fed16cfceeec335825e677506b6bcaae
dannypsnl <dannypsnl@gmail.com>
2018-01-08 11:52:43 +0800

    fixed the bug, first  element contains 2 & improve doc comment

--- c++/data-structure/list/list.h
+++ c++/data-structure/list/list.h
@@ -3,9 +3,11 @@
 
 template <class T> class Node {
   T value;
+  // next_node default is nullptr
   Node *next_node = nullptr;
 
 public:
+  // Just because compiler won't create one for you
   Node();
   Node(T);
   void next(Node<T> *);
@@ -24,7 +26,7 @@ template <class T> T Node<T>::get_value() { return value; }
 template <class T> Node<T> *Node<T>::get_next() { return next_node; }
 
 template <class T> class List {
-  Node<T> *first = new Node<T>();
+  Node<T> *first;
 
 public:
   List(std::initializer_list<T>);
@@ -32,9 +34,10 @@ public:
 };
 
 template <class T> List<T>::List(std::initializer_list<T> l) {
-  first = new Node<T>(*l.begin());
+  auto iter = l.begin();
+  first = new Node<T>(*iter);
   auto now = first;
-  for (auto iter = l.begin(); iter != l.end(); ++iter) {
+  for (iter = ++iter; iter != l.end(); ++iter) {
     now->next(new Node<T>(*iter));
     now = now->get_next();
   }
--- c++/data-structure/list/main.cc
+++ c++/data-structure/list/main.cc
@@ -3,13 +3,6 @@
 #include <iostream>
 
 int main() {
-  Node<int> first = Node<int>(10);
-  Node<int> second = Node<int>(20);
-  first.next(&second);
-
-  std::cout << first.get_value() << std::endl;
-  std::cout << first.get_next()->get_value() << std::endl;
-
   List<int> l{1,  2,  3,  5, 6, 7, 7, 87, 87, 8, 78, 7,
               35, 23, 23, 5, 4, 2, 5, 89, 1,  6, 6,  4};
   l.for_each();
