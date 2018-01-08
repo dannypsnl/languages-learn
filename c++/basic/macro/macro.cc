7986cb4c389a864fba91fc54b68801b937477678
dannypsnl <dannypsnl@gmail.com>
2018-01-08 14:52:05 +0800

    update macro trick

--- c++/basic/macro/macro.cc
+++ c++/basic/macro/macro.cc
@@ -1,7 +1,10 @@
 #include <iostream>
 
-#define NEW_FUNC(__name, __id) /* append __name and __id */                    \
-  void __name##__id() { std::cout << "func: " << __id << std::endl; }
+#define NEW_FUNC(__name,                                                       \
+                 __id) /* append __name and __id to be function name */        \
+  void __name##__id() {                                                        \
+    std::cout << "func_" << #__name ": " << #__id << std::endl;                \
+  }
 
 NEW_FUNC(foo, 1) // foo1
 NEW_FUNC(foo, 2) // foo2
