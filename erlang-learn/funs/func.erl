08bb298bf3da8c4420a805685ee809332eea8e7a
dannypsnl <dannypsnl@gmail.com>
2018-01-09 13:10:37 +0800

    create Hypot, TempConvert func

--- erlang-learn/funs/func.erl
+++ erlang-learn/funs/func.erl
@@ -4,4 +4,12 @@
 test() ->
     Double = fun(X) -> 2*X end,
     4 = Double(2),
+    Hypot = fun(X, Y) -> math:sqrt(X*X + Y*Y) end,
+    5.0 = Hypot(3, 4),
+    TempConvert = fun({c, C}) -> {f, 32+C*9/5};
+                     ({f, F}) -> {c, (F-32)*5/9}
+                  end,
+    {f, 212.0} = TempConvert({c, 100}),
+    {f, 32.0} = TempConvert({c, 0}),
+    {c, 100.0} = TempConvert({f, 212}),
     test_pass.
