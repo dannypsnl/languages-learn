2840fae1b925e207bd4269d5f939005bdcfb808a
dannypsnl <dannypsnl@gmail.com>
2018-01-09 14:27:52 +0800

    add macro protect, spread implement into cpp file

--- c++/basic/data-structure/list/list.h
+++ c++/basic/data-structure/list/list.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <initializer_list>
 #include <iostream>
 
@@ -19,16 +21,6 @@ public:
   Node<T> *get_next();
 };
 
-template <class T> Node<T>::Node() {}
-
-template <class T> Node<T>::Node(T v) : value{v} {}
-
-template <class T> void Node<T>::next(Node *n) { this->next_node = n; }
-
-template <class T> T Node<T>::get_value() { return value; }
-
-template <class T> Node<T> *Node<T>::get_next() { return next_node; }
-
 } // namespace
 
 template <class T> class List {
@@ -39,21 +31,6 @@ public:
   void for_each();
 };
 
-template <class T> List<T>::List(std::initializer_list<T> l) {
-  auto iter = l.begin();
-  first = new Node<T>(*iter);
-  auto now = first;
-  for (iter = ++iter; iter != l.end(); ++iter) {
-    now->next(new Node<T>(*iter));
-    now = now->get_next();
-  }
-}
-
-// TODO: accept functor at for_each
-template <class T> void List<T>::for_each() {
-  for (auto now = first; now->get_next() != nullptr; now = now->get_next()) {
-    std::cout << now->get_value() << std::endl;
-  }
-}
-
 } // namespace collec
+
+#include "./list.cpp"
