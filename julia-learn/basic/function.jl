bd904c5a55c3e0322d6d0388e4877ff076931890
dannypsnl <dannypsnl@gmail.com>
2018-01-12 13:52:29 +0800

    hypot

--- julia-learn/basic/function.jl
+++ julia-learn/basic/function.jl
@@ -5,3 +5,27 @@ end
 
 println(f(1, 2))
 # 3
+g = f # g is f
+println(g(1, 2))
+
+function with_return()
+    return "I have return"
+end
+println(with_return())
+
+function hypot(x, y)
+    x = abs(x)
+    y = abs(y)
+    if x > y
+        r = y/x
+        return x*sqrt(1+r*r)
+    end
+    if y == 0
+        return zero(x)
+    end
+    r = x/y
+    return y*sqrt(1+r*r)
+end
+
+println(hypot(3, 4))
+# 5
