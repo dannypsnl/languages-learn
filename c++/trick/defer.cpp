d3bf2b74c1ddc143a22cf7d0ecf8c2711159e50c
dannypsnl <dannypsnl@gmail.com>
2018-01-18 17:13:04 +0800

    [impl]defer complete

--- c++/trick/defer.cpp
+++ c++/trick/defer.cpp
@@ -1,15 +1,36 @@
 #include <functional>
 #include <iostream>
+#include <stack>
 
-template <class T> class defer {
-  T defered;
+class deferer {
+  std::stack<std::function<void()>> defers;
+  void callAll() {
+    while (!this->defers.empty()) {
+
+      this->defers.top()();
+      this->defers.pop();
+    }
+  }
 
 public:
-  defer(T f) : defered{f} {}
-  ~defer() { this->defered(); }
+  deferer() {}
+  void add(std::function<void()> &&f) {
+    this->defers.push(std::forward<decltype(f)>(f));
+  }
+  ~deferer() { callAll(); }
 };
 
+#define allow_defer() deferer __deferer{};
+#define defer(...)                                                             \
+  auto defered = std::bind(__VA_ARGS__);                                       \
+  __deferer.add(defered);
+
 int main() {
-  defer<std::function<void()>> d{[]() { std::cout << "defer!!!" << '\n'; }};
+  allow_defer();
+  defer([=]() { std::cout << "defer!!!!!!!!!!" << '\n'; });
+  for (size_t i = 0; i < 10; ++i) {
+    defer(
+        [=]() { std::cout << "Number " << i + 1 << " defer be call" << '\n'; });
+  }
   std::cout << "hello, defer" << '\n';
 }
