3733995b60e3f5c4fb0d7cf4f6104ab173b5a6bd
routedan <dannypsnl@gmail.com>
2018-01-20 21:42:24 +0800

    gcd read args

--- rust-learn/basic/gcd.rs
+++ rust-learn/basic/gcd.rs
@@ -11,6 +11,25 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
     n
 }
 
+use std::io::Write;
+use std::str::FromStr;
+
 fn main() {
-    println!("gcd(14, 15) = {}", gcd(14, 15));
+    let mut numbers = Vec::new();
+
+    for arg in std::env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("can't parse this arg"));
+    }
+
+    if numbers.len() == 0 {
+        writeln!(std::io::stderr(), "Usage: gcd NUMBER ...").unwrap();
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
 }
