4aaf3a54d1c62766d5a45dd903b519ceae14fa53
dannypsnl <dannypsnl@gmail.com>
2018-04-02 22:42:19 +0800

    add: print | line

--- perl-learn/basic/sudoku.pl
+++ perl-learn/basic/sudoku.pl
@@ -2,6 +2,12 @@ use v6;
 my $sudoku = '000000075000080094000500600010000200009000570060030400010000230800000006063240000';
 $sudoku = $sudoku.trans('0' => ' ');
 
-for 0..8 -> $line-number {
-    say substr $sudoku, $line-number * 9, 9;
+sub chunks(Str $s, Int $chars) {
+    gather loop (my $idx = 0; $idx < $s.chars; $idx += $chars) {
+        take substr($s, $idx, $chars);
+    }
+}
+
+for chunks($sudoku, 9) -> $line {
+    say chunks($line, 3).join('|');
 }
