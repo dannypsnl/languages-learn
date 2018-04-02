13927559159f795e5218ea260c2b6ae59b21a737
dannypsnl <dannypsnl@gmail.com>
2018-04-02 22:55:20 +0800

    add: MAIN & use args to create a sudoku

--- perl-learn/basic/sudoku.pl
+++ perl-learn/basic/sudoku.pl
@@ -1,10 +1,14 @@
 use v6;
-my $sudoku = '000000075000080094000500600010000200009000570060030400010000230800000006063240000';
-$sudoku = $sudoku.trans: '0' => ' ';
 
-my $separator = '+---+---+---+';
-for $sudoku.comb(9) -> $line {
-    say $separator if $++ %% 3;
-    say '|', $line.comb(3).join('|'), '|';
+# 000000075000080094000500600010000200009000570060030400010000230800000006063240000
+constant separator = '+---+---+---+';
+
+sub MAIN($sudoku) {
+    my $substituted = $sudoku.trans: '0' => ' ';
+
+    for $substituted.comb(9) -> $line {
+        say separator if $++ %% 3;
+        say '|', $line.comb(3).join('|'), '|';
+    }
+    say separator;
 }
-say $separator;
