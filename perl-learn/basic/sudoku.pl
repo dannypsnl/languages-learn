e109155d1221cdee89032031b16391d68bdbd8f4
dannypsnl <dannypsnl@gmail.com>
2018-04-02 22:47:35 +0800

    refactor: use built-in instead of chunks

--- perl-learn/basic/sudoku.pl
+++ perl-learn/basic/sudoku.pl
@@ -1,16 +1,10 @@
 use v6;
 my $sudoku = '000000075000080094000500600010000200009000570060030400010000230800000006063240000';
-$sudoku = $sudoku.trans('0' => ' ');
-
-sub chunks(Str $s, Int $chars) {
-    gather loop (my $idx = 0; $idx < $s.chars; $idx += $chars) {
-        take substr($s, $idx, $chars);
-    }
-}
+$sudoku = $sudoku.trans: '0' => ' ';
 
 my $separator = '+---+---+---+';
-for chunks($sudoku, 9) -> $line {
+for $sudoku.comb(9) -> $line {
     say $separator if $++ %% 3;
-    say '|', chunks($line, 3).join('|'), '|';
+    say '|', $line.comb(3).join('|'), '|';
 }
 say $separator;
