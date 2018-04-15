293af3df336395ba948ac0140fe1c432bf6e55bd
dannypsnl <dannypsnl@gmail.com>
2018-04-15 22:11:47 +0800

    add: object ref, nim

--- nim/basic/structure.nim
+++ nim/basic/structure.nim
@@ -4,6 +4,10 @@ type
   Person = object
     name*: string # Field is exported using `*`.
     age: Natural  # Natural type ensures the age is positive.
+  PersonRef = ref Person
+
+proc rename(person: PersonRef, name: string) =
+  person.name = name
 
 var people = [
   Person(name: "John", age: 45),
@@ -12,3 +16,7 @@ var people = [
 
 for person in people:
   echo("$1 is $2 years old" % [person.name, $person.age])
+
+var dan = PersonRef(name: "Dan", age: 20)
+dan.rename("Danny")
+echo(dan.name)
