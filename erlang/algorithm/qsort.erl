23e3a809d05192109005521dc1b9c793d59c9636
dannypsnl <dannypsnl@gmail.com>
2018-12-30 01:42:52 +0800

    test: quick sort

--- erlang/algorithm/qsort.erl
+++ erlang/algorithm/qsort.erl
@@ -1,5 +1,5 @@
 -module(qsort).
--export([sort/1]).
+-export([sort/1, test/0]).
 
 % improve invoke look
 sort(L) -> qsort(L).
@@ -7,10 +7,15 @@ sort(L) -> qsort(L).
 % No item, no need to sort
 qsort([]) -> [];
 % H is first element
-qsort([H|L]) ->
+qsort([Head|L]) ->
 	% the list of smaller item(compare with H)
-	qsort([X || X <- L, X < H])
-	++ [H] ++
+	qsort([Smaller || Smaller <- L, Smaller < Head])
+	++ [Head] ++
 	% the list of greater|equal item(compare with H)
-	qsort([X || X <- L, X >= H])
+	qsort([Larger || Larger <- L, Larger >= Head])
+	.
+
+test() ->
+	[1, 2, 3] = qsort([3, 2, 1]),
+	ok
 	.
