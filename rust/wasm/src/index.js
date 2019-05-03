50ba4bdc4f1451f872d660d45e3f9070cf3b803e
dannypsnl <dannypsnl@gmail.com>
2019-05-03 09:25:17 +0800

    using parcel replacing webpack

--- rust/wasm/src/index.js
+++ rust/wasm/src/index.js
@@ -1,9 +1,2 @@
-const wasm = require('./main.rs');
-
-wasm.initialize({noExitRuntime: true}).then(module => {
-    // the ref to us wasm fn, fn name, rt type, input types
-    const add = module.cwrap('add', 'number', ['number', 'number']);
-
-    console.log('Calling rust functions from javascript!');
-    console.log('Rust add(1, 3) =', add(1, 2));
-})
+import {add} from './lib.rs';
+console.log('Rust add(1, 3) =', add(1, 3));
--- rust/wasm/src/lib.rs
+++ rust/wasm/src/lib.rs
@@ -1,7 +1,3 @@
-fn main() {
-    println!("Hello, world!");
-}
-
 #[no_mangle]
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
