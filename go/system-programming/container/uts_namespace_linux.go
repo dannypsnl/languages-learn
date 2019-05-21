7b6647d27ce679d9850ca14a74ffc3688e2021d4
dannypsnl <dannypsnl@gmail.com>
2019-05-21 22:05:09 +0800

    new UTS namespace by changing sysProcAttr of command

---
+++ go/system-programming/container/uts_namespace_linux.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	cmd := exec.Command("sh")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("failed at running command: %s", err)
+	}
+}
