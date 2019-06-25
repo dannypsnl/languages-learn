c836015d0dd8697c896724b23169608eabf042b0
dannypsnl <dannypsnl@gmail.com>
2019-06-25 10:03:04 +0800

    fake dns

---
+++ go/system-programming/networking/dns/fakedns/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"net"
+	"strconv"
+
+	"github.com/miekg/dns"
+)
+
+var domainsToAddresses map[string]string = map[string]string{
+	"test.service": "10.248.0.7",
+}
+
+type handler struct{}
+
+func (this *handler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
+	msg := dns.Msg{}
+	msg.SetReply(r)
+	switch r.Question[0].Qtype {
+	case dns.TypeA:
+		msg.Authoritative = true
+		domain := msg.Question[0].Name
+		address, ok := domainsToAddresses[domain]
+		if ok {
+			msg.Answer = append(msg.Answer, &dns.A{
+				Hdr: dns.RR_Header{Name: domain, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 60},
+				A:   net.ParseIP(address),
+			})
+		}
+	}
+	w.WriteMsg(&msg)
+}
+
+func main() {
+	srv := &dns.Server{Addr: ":" + strconv.Itoa(53), Net: "udp"}
+	srv.Handler = &handler{}
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf("Failed to set udp listener %s\n", err.Error())
+	}
+}
