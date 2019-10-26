1c03126f7de5283f7857abe19b96b721f3e9088b
dannypsnl <dannypsnl@gmail.com>
2019-10-26 11:03:02 +0800

    exercise 1.6

--- racket/sicp/sqrt.rkt
+++ racket/sicp/sqrt.rkt
@@ -23,3 +23,16 @@
 (sqrt (+ 100 37))
 (sqrt (+ (sqrt 2) (sqrt 3)))
 (square (sqrt 1000))
+
+;;; Exercise 1.6
+(define (new-if pred thenE elseE)
+  (cond (pred thenE)
+        (else elseE)))
+(define (sqrt-iter2 guess x)
+  (new-if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x) x)))
+(define (sqrt2 x)
+  (sqrt-iter2 1.0 x))
+
+(sqrt2 9)
